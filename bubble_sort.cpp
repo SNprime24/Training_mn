5039fa19d304c67aba39a2aa3607f155bd49a1bd
Suprit <supritnaik100@gmail.com>
2026-07-14 11:18:15 +0530

    DEBUG and FIX bubble_sort logic to prevent out-of-bounds access

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,6 +1,6 @@
 void bubble_sort(int arr[], int n){
     for(int i=0; i<n; i++){
-        for(int j=0; j<n-i; j++){
+        for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
